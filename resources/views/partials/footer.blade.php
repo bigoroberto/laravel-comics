bf68b6bb670c67f75fb15b166ee17192cd9fb1dd
bigoroberto <bigoroberto@gmail.com>
2021-06-22 18:48:46 +0200

    non va niente

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,93 @@
+<footer>
+  <div class="container footer-top"> 
+    <div class="list">
+  
+      <div> 
+        <h5>DC COMICS</h5>
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">news</a></li>
+        </ul>
+        <h5>SHOP</h5>
+        <ul>
+          <li><a href="#">Shop DC</a></li>
+          <li><a href="#">Shop DC Collectibles</a></li>
+        </ul>
+      </div>
+      <div> 
+        <h5>DC</h5>
+        <ul> 
+          <li><a href="#">Terms Of Use</a></li>
+          <li><a href="#">Privacy Policy (New)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent workshops</a></li>
+          <li><a href="#">CPSC certificates</a></li>
+          <li><a href="#">ratings</a></li>
+          <li><a href="#">shop help</a></li>
+          <li><a href="#">contact us</a></li>
+        </ul>
+      </div>
+      <div>
+        <h5>SITES</h5>
+        <ul>
+          <li><a href="/">DC</a></li>
+          <li><a href="#">MAD Magazine</a></li>
+          <li><a href="#">DC kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC power visa</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-logo">
+
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+    <div class="footer-button">
+      <button>
+        <a href="#">SIGN UP NOW!</a>
+      </button>
+    </div>
+    <div  class="socials">
+      <span><h5>FOLLOW US</h5></span>
+      <ul>
+        <li>
+          <a href="#">
+            <img src="asset('img/footer-facebook.png')" alt="">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="asset('img/footer-twitter.png')" alt="">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="asset('img/footer-youtube.png')" alt="">
+           </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="asset('img/footer-pinterest.png')" alt="">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="asset('img/footer-periscope.png')" alt="">
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header class="container">
+  <div class="container-logo">
+    <a href="/">
+      <img src="../assets/img/dc-logo.png" alt="Logo">
+    </a>
+  </div>
+
+  <div class="container-nav">
+    <nav>
+      <ul>
+        <li>
+          <a class="{'active': link.current}" :href=""></a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header> 
